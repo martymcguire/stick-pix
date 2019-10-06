80535714b2744b78c436b9807ae91c19213e3607
Marty McGuire <schmartissimo@gmail.com>
2019-10-06 14:36:01 -0400

    add snapshot button to get saveable images

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d')
 const tracker = new tracking.ObjectTracker('face')
+const video = document.getElementById('video');
 const flowerCrownButton = document.getElementById('flower-crown')
 const bunnyEarsButton = document.getElementById('bunny-ears')
+let trackData = [];
 
 const img = new Image()
 let filterX = 0
@@ -32,16 +34,41 @@ bunnyEarsButton.addEventListener('click', () => {
 
 tracker.setInitialScale(4)
 tracker.setStepSize(2)
-tracker.setEdgesDensity(0.1)
-tracking.track('#video', tracker, { camera: true })
+tracker.setEdgesDensity(0.075)
+const activeTracker = tracking.track('#video', tracker, { camera: true })
 
 tracker.on('track', event => {
   context.clearRect(0, 0, canvas.width, canvas.height)
-  event.data.forEach(rect => {
-    context.drawImage(img, rect.x + (filterX * rect.width),
+  trackData = event.data;
+  drawOverlays(trackData, context, img);
+})
+
+function drawOverlays(rects, ctx) {
+  rects.forEach(rect =>{
+    ctx.drawImage(img, rect.x + (filterX * rect.width),
     rect.y + (filterY * rect.height),
     rect.width * filterWidth,
     rect.height * filterHeight
   )
-  })
-})
+  });
+}
+
+function saveImage() {
+  activeTracker.stop();
+  const liveControls = document.getElementById('liveControls');
+  const saveImg = document.getElementById('saveImage');
+  const saveCanvas = document.createElement('canvas');
+  const saveCtx = saveCanvas.getContext('2d');
+  saveCanvas.width = video.width;
+  saveCanvas.height = video.height;
+  saveCtx.drawImage(video, 0, 0, saveCanvas.width, saveCanvas.height);
+  drawOverlays(trackData, saveCtx);
+  saveImg.src = saveCanvas.toDataURL('image/jpeg');
+  liveControls.classList.add('hidden');
+}
+
+function tryAgain() {
+  const liveControls = document.getElementById('liveControls');
+  liveControls.classList.remove('hidden');
+  activeTracker.run();
+}
